434909254cee0920e1b88f067eb81963d72f2c8c
doomds <de.simone.dc@gmail.com>
2026-05-19 21:35:42 +0200

    feat: add branch class

---
+++ src/main/java/com/doom/section10_lists/practice/banking_branch_exo/Branch.java
@@ -0,0 +1,38 @@
+package com.doom.section10_lists.practice.banking_branch_exo;
+
+import java.util.ArrayList;
+
+public class Branch {
+    String name;
+    ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean newCustomer(String name, double initialTransaction) {
+        return newCustomer(name, initialTransaction);
+    }
+
+    public boolean addCustomerTransaction(String name, double transaction) {
+        return addCustomerTransaction(name, transaction);
+    }
+
+    public Customer findCustomer(String name) {
+        for (Customer customer : customers) {
+            if (customers.contains(customer)) {
+                return customer;
+            }
+        }
+        return null;
+    }
+}
