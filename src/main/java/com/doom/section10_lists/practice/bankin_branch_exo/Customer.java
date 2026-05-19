9b3ebeb5f9aec4301a5bf52c85dc744ed74ee19c
doomds <de.simone.dc@gmail.com>
2026-05-19 14:56:10 +0200

    feat: add banking_branch exercice

---
+++ src/main/java/com/doom/section10_lists/practice/bankin_branch_exo/Customer.java
@@ -0,0 +1,15 @@
+package com.doom.section10_lists.practice.bankin_branch_exo;
+
+import java.util.ArrayList;
+
+public class Customer {
+
+    String name;
+    ArrayList<Double> transactions;
+
+    public Customer(String name, double initialDeposit) {
+        this.name = name.toUpperCase();
+        this.transactions = new ArrayList<>(500);
+        transactions.add(initialDeposit);
+    }
+}
