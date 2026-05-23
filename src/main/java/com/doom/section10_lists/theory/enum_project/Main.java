3e4ccfa5fa2dcdf5fca7657074d73bc882e3ef93
doomds <de.simone.dc@gmail.com>
2026-05-23 23:14:45 +0200

    feat: add some tests in main for enum

--- src/main/java/com/doom/section10_lists/theory/enum_project/Main.java
+++ src/main/java/com/doom/section10_lists/theory/enum_project/Main.java
@@ -1,8 +1,15 @@
 package com.doom.section10_lists.theory.enum_project;
 
+import java.time.DayOfWeek;
+
 public class Main {
     public static void main(String[] args) {
 
-    }
+        DayOfWeek[] dow = DayOfWeek.values();
 
+        for (DayOfTheWeek dow2 : DayOfTheWeek.values()) {
+            System.out.printf("Name is %s and ordinal is %s\n",
+                    dow2.name(), dow2.ordinal());
+        }
+    }
 }
