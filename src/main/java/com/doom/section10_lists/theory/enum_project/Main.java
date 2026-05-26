3f299e1b896c62e70d0679dce586517eb087a5f8
doomds <de.simone.dc@gmail.com>
2026-05-26 15:00:41 +0200

    feat: add switchDay method

--- src/main/java/com/doom/section10_lists/theory/enum_project/Main.java
+++ src/main/java/com/doom/section10_lists/theory/enum_project/Main.java
@@ -25,4 +25,17 @@ public class Main {
 
         return allDays[randomInteger];
     }
+
+    public static void switchDay(DayOfTheWeek dow) {
+        int weekDayInteger = dow.ordinal() + 1;
+        switch (dow) {
+            case MON -> System.out.println("Monday is Day " + weekDayInteger);
+            case TUES ->  System.out.println("Tuesday is Day " + weekDayInteger);
+            case WED -> System.out.println("Wednesday is Day " +  weekDayInteger);
+            case THURS -> System.out.println("Thursday is Day " +  weekDayInteger);
+            case FRI ->  System.out.println("Friday is Day " +  weekDayInteger);
+            case SAT ->  System.out.println("Saturday is Day " +  weekDayInteger);
+            case SUN -> System.out.println("Sunday is Day " +  weekDayInteger);
+        }
+    }
 }
