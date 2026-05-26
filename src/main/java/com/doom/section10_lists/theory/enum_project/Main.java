8133f5845d8650d21bc16b6a5967cd49b0329ca5
doomds <de.simone.dc@gmail.com>
2026-05-26 14:50:33 +0200

    feat: DayOfTheWeek method for enum

--- src/main/java/com/doom/section10_lists/theory/enum_project/Main.java
+++ src/main/java/com/doom/section10_lists/theory/enum_project/Main.java
@@ -1,15 +1,25 @@
 package com.doom.section10_lists.theory.enum_project;
 
 import java.time.DayOfWeek;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
 
-        DayOfWeek[] dow = DayOfWeek.values();
+        DayOfTheWeek dow = DayOfTheWeek.TUES;
 
-        for (DayOfTheWeek dow2 : DayOfTheWeek.values()) {
+        for (int i = 0; i < 10; i++) {
+            dow = getRandomDay();
             System.out.printf("Name is %s and ordinal is %s\n",
-                    dow2.name(), dow2.ordinal());
+                    dow.name(),
+                    dow.ordinal());
         }
+    }
+
+    public static DayOfTheWeek getRandomDay() {
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+
+        return allDays[randomInteger];
     }
 }
