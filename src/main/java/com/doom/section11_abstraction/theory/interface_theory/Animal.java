cf4e4e4a363ddf545816dd044e8ad3a7dbe43077
doomds <de.simone.dc@gmail.com>
2026-06-08 14:50:38 +0200

    feat: add interface_theory package

---
+++ src/main/java/com/doom/section11_abstraction/theory/interface_theory/Animal.java
@@ -0,0 +1,8 @@
+package com.doom.section11_abstraction.theory.interface_theory;
+
+interface FlightEnabled {}
+
+interface Trackable {}
+
+public abstract class Animal {
+}
---
+++ src/main/java/com/doom/section11_abstraction/theory/interface_theory/Bird.java
@@ -0,0 +1,4 @@
+package com.doom.section11_abstraction.theory.interface_theory;
+
+public class Bird extends Animal implements FlightEnabled, Trackable {
+}
