854e2aee0c2efc17c52947a6f61d23841df50b7a
doomds <de.simone.dc@gmail.com>
2026-06-11 14:57:04 +0200

    feat: complete ListItem classe

--- src/main/java/com/doom/section11_abstraction/practice/exercice_47/ListItem.java
+++ src/main/java/com/doom/section11_abstraction/practice/exercice_47/ListItem.java
@@ -1,4 +1,25 @@
 package com.doom.section11_abstraction.practice.exercice_47;
 
 public abstract class ListItem {
+    protected ListItem rightLink;
+    protected ListItem leftLink;
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+
+    abstract void next();
+    abstract void setNext();
+    abstract void previous();
+    abstract void setPrevious();
+    abstract void compareTo();
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
 }
