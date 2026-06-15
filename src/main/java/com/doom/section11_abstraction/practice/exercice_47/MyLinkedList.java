fb474987c34befbf059c9101da6e1960df240c06
doomds <de.simone.dc@gmail.com>
2026-06-15 11:06:52 +0200

    feat: add MyLinkedList methods

--- src/main/java/com/doom/section11_abstraction/practice/exercice_47/MyLinkedList.java
+++ src/main/java/com/doom/section11_abstraction/practice/exercice_47/MyLinkedList.java
@@ -1,4 +1,29 @@
 package com.doom.section11_abstraction.practice.exercice_47;
 
-public class MyLinkedList {
+public class MyLinkedList implements NodeList {
+    private ListItem root;
+
+    public MyLinkedList(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return null;
+    }
+
+    @Override
+    public void addItem() {
+
+    }
+
+    @Override
+    public void removeItem() {
+
+    }
+
+    @Override
+    public void traverse() {
+
+    }
 }
