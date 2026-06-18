c9cbf59ce03585a12f7774f0a49e498d6b1e6354
doomds <de.simone.dc@gmail.com>
2026-06-18 14:57:44 +0200

    feat: Creating first generic classe

---
+++ src/main/java/com/doom/section12_generics/theory/Main.java
@@ -0,0 +1,17 @@
+package com.doom.section12_generics.theory;
+
+record BaseballPlayer(String name, String position) {}
+
+public class Main {
+    public static void main(String[] args) {
+        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros = new BaseballTeam("Houston Astros");
+        scoreResult(phillies, 3, astros, 5);
+    }
+
+    private static void scoreResult(BaseballTeam team1, int t1_score, BaseballTeam team2, int t2_score) {
+        String message = team1.setScore(t1_score, t2_score);
+        team2.setScore(t2_score, t1_score);
+        System.out.printf("%s %s %s %n", team1, message, team2);
+    }
+}
